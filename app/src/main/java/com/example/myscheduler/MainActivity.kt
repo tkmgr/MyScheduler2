622cd460b996fbe831734d91611361ae48a8dc4e
tkmgr <taskmgrdev@gmail.com>
2019-05-26 22:15:04 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myscheduler/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.myscheduler
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.realm.Realm
+import io.realm.kotlin.where
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var realm:Realm
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        realm = Realm.getDefaultInstance()
+
+        list.layoutManager = LinearLayoutManager(this)
+        val schedules  = realm.where<Schedule>().findAll()
+        val adapter = ScheduleAdapter(schedules)
+        list.adapter = adapter
+
+        fab.setOnClickListener { view ->
+            val  intent = Intent(this,ScheduleEditActivity::class.java)
+            startActivity(intent)
+        }
+
+        adapter.setOnItemClickListener { id ->
+            val intent = Intent(this, ScheduleEditActivity::class.java)
+                .putExtra("schedule_id",id)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myscheduler/ScheduleAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.myscheduler
+
+import android.text.format.DateFormat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import io.realm.OrderedCollectionChangeSet
+import io.realm.OrderedRealmCollection
+import io.realm.RealmRecyclerViewAdapter
+
+class ScheduleAdapter(data: OrderedRealmCollection<Schedule>) :
+    RealmRecyclerViewAdapter<Schedule,ScheduleAdapter.ViewHolder>(data,true) {
+
+    private var listener:((Long?)-> Unit)? = null
+
+    fun setOnItemClickListener(listener:(Long?)-> Unit){
+        this.listener = listener
+    }
+
+
+    init {
+        setHasStableIds(true)
+    }
+
+    class ViewHolder(cell: View) : RecyclerView.ViewHolder(cell){
+        val date: TextView = cell.findViewById(android.R.id.text1)
+        val title: TextView = cell.findViewById(android.R.id.text2)
+    }
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScheduleAdapter.ViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val view = inflater.inflate(android.R.layout.simple_list_item_2,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ScheduleAdapter.ViewHolder, position: Int) {
+        val schedule: Schedule? = getItem(position)
+        holder.date.text = DateFormat.format("yyyy/MM/dd",schedule?.date)
+        holder.title.text = schedule?.title
+        holder.itemView.setOnClickListener {
+            listener?.invoke(schedule?.id)
+        }
+    }
+
+    override fun getItemId(position: Int):Long{
+        return getItem(position)?.id ?:0
+    }
+}
